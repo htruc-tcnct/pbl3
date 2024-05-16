22b1875ef373fd3d4f8e30e88f8bc238c1d962fa
htruc-tcnct <truc95520@gmail.com>
2024-05-16 23:48:30 +0700

    push code

---
+++ BusinessLayer/SalaryCalculator.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.DTO;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLayer
+{
+    public class SalaryCalculator
+    {
+        private PBL3Entities db = new PBL3Entities();
+
+        public SalaryDTO CalculateSalary(string employeeId, int month, int year)
+        {
+            var wage = db.tb_Wage.FirstOrDefault(x => x.EmployeeID == employeeId);
+
+            var overtimes = db.tb_OverTime
+                .Where(x => x.EmployeeID == employeeId && x.OvertimeDate.HasValue && x.OvertimeDate.Value.Month == month && x.OvertimeDate.Value.Year == year)
+                .ToList();
+
+            var sickOffs = db.tb_SickOff
+                .Where(x => x.EmployeeID == employeeId && x.SickDate.HasValue && x.SickDate.Value.Month == month && x.SickDate.Value.Year == year)
+                .ToList();
+
+            double basicSalary = Convert.ToDouble(wage.Wage);
+            int overtimeHours = overtimes.Sum(x => x.OvertimeHours ?? 0); // Sử dụng toán tử '??' để xử lý giá trị null
+            double overtimeRate = overtimes.Any() ? overtimes.Average(x => x.coeffSalary ?? 0) : 0; // Sử dụng '??' cho giá trị nullable double
+            double latePenalty = 0; // Giả sử bạn có cách tính phạt trễ giờ làm
+            int sickDays = sickOffs.Count;
+
+            double dailySalary = basicSalary / 22; // Giả sử có 22 ngày làm việc trong tháng
+            double totalSalary = basicSalary + (overtimeHours * overtimeRate * dailySalary) - latePenalty - (sickDays * dailySalary);
+
+            return new SalaryDTO
+            {
+                BasicSalary = basicSalary,
+                OvertimeHours = overtimeHours,
+                LatePenalty = latePenalty,
+                SickDays = sickDays,
+                TotalSalary = totalSalary
+            };
+        }
+    }
+}
---
+++ GUI/EMPLOYEE/tinhluong/tinhluong.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace GUI.EMPLOYEE.payroll
+{
+    partial class tinhluong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtBasicSalary = new System.Windows.Forms.TextBox();
+            this.txtOvertimeHours = new System.Windows.Forms.TextBox();
+            this.txtLatePenalty = new System.Windows.Forms.TextBox();
+            this.txtSickDays = new System.Windows.Forms.TextBox();
+            this.txtTotalSalary = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn thời gian ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(431, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bảng lương tháng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "lương cơ bản";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(275, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "số giờ tăng ca";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(275, 317);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Phạt trễ giờ làm ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(275, 398);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Số ngày nghỉ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(275, 487);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Tổng lương";
+            // 
+            // txtBasicSalary
+            // 
+            this.txtBasicSalary.Location = new System.Drawing.Point(434, 151);
+            this.txtBasicSalary.Name = "txtBasicSalary";
+            this.txtBasicSalary.Size = new System.Drawing.Size(203, 22);
+            this.txtBasicSalary.TabIndex = 7;
+            // 
+            // txtOvertimeHours
+            // 
+            this.txtOvertimeHours.Location = new System.Drawing.Point(434, 229);
+            this.txtOvertimeHours.Name = "txtOvertimeHours";
+            this.txtOvertimeHours.Size = new System.Drawing.Size(203, 22);
+            this.txtOvertimeHours.TabIndex = 8;
+            // 
+            // txtLatePenalty
+            // 
+            this.txtLatePenalty.Location = new System.Drawing.Point(434, 317);
+            this.txtLatePenalty.Name = "txtLatePenalty";
+            this.txtLatePenalty.Size = new System.Drawing.Size(203, 22);
+            this.txtLatePenalty.TabIndex = 9;
+            // 
+            // txtSickDays
+            // 
+            this.txtSickDays.Location = new System.Drawing.Point(434, 395);
+            this.txtSickDays.Name = "txtSickDays";
+            this.txtSickDays.Size = new System.Drawing.Size(203, 22);
+            this.txtSickDays.TabIndex = 10;
+            // 
+            // txtTotalSalary
+            // 
+            this.txtTotalSalary.Location = new System.Drawing.Point(434, 481);
+            this.txtTotalSalary.Name = "txtTotalSalary";
+            this.txtTotalSalary.Size = new System.Drawing.Size(203, 22);
+            this.txtTotalSalary.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(166, 59);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 12;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged_1);
+            // 
+            // tinhluong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 599);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtTotalSalary);
+            this.Controls.Add(this.txtSickDays);
+            this.Controls.Add(this.txtLatePenalty);
+            this.Controls.Add(this.txtOvertimeHours);
+            this.Controls.Add(this.txtBasicSalary);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "tinhluong";
+            this.Text = "payroll";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtBasicSalary;
+        private System.Windows.Forms.TextBox txtOvertimeHours;
+        private System.Windows.Forms.TextBox txtLatePenalty;
+        private System.Windows.Forms.TextBox txtSickDays;
+        private System.Windows.Forms.TextBox txtTotalSalary;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
